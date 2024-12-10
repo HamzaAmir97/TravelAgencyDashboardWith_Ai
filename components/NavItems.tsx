ba4860fce03183c820cca6170b3e3491dc7a7cfa
Hmazah <alhamza2012@gmail.com>
2024-12-10 10:00:00 +0200

    add project folder with all packages

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import {Link, NavLink, useLoaderData, useNavigate} from "react-router";
+import {sidebarItems} from "~/constants";
 
-function NavItems() {
-  return (
-    <div>NavItems</div>
-  )
+
+const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
+
+
+    return (
+        <section className="nav-items">
+            <Link to='/' className="link-logo">
+                <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+                <h1>Tourvisto</h1>
+            </Link>
+
+        </section>
+    )
 }
 
 export default NavItems
