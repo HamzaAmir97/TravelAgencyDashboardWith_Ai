e3e20e4c44ed4ad0bf6009845629542f157b20e6
Hmazah <alhamza2012@gmail.com>
2024-12-27 10:00:00 +0200

    add project folder with all packages

--- app/routes.ts
+++ app/routes.ts
@@ -1,6 +1,7 @@
 import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
 
 export default [
+    route('sign_in', 'routes/root/sign_in.tsx'),
     layout('/routes/admin/admin_layout.tsx',[
 
         route('dashboard', 'routes/admin/dashboard.tsx') 
---
+++ app/routes/root/sign_in.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sign_in() {
+  return (
+    <div>sign-in</div>
+  )
+}
+
+export default sign_in
