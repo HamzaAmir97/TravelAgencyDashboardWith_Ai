10f78319da02923740131c29fc502f2944924ce0
Hmazah <alhamza2012@gmail.com>
2025-01-09 10:00:00 +0200

    add sentry

--- app/root.tsx
+++ app/root.tsx
@@ -10,8 +10,9 @@ import {
 } from "react-router";
 
 import * as Sentry from "@sentry/react-router";
-import type { Route } from "./+types/root";
+
 import "./app.css";
+import type { Route } from "./routes/+types/home";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
