465ccfbd1a2c798cdfdb71d079d7770fce5faff1
Hmazah <alhamza2012@gmail.com>
2025-01-19 10:00:00 +0200

    add  referrerPolicy='no-referrer'

--- instrument.server.mjs
+++ instrument.server.mjs
@@ -2,13 +2,13 @@ import * as Sentry from "@sentry/react-router";
 import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 Sentry.init({
-  dsn: "https://9cf842170f8792d4937c9a530ca06ec3@o4509508073947136.ingest.de.sentry.io/4509508078272592",
-  
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-  
-  integrations: [nodeProfilingIntegration()],
-  tracesSampleRate: 1.0, // Capture 100% of the transactions
-  profilesSampleRate: 1.0, // profile every transaction
+ dsn: "https://5490ef8c6bcb638779d99e990baa9a90@o4509212149612544.ingest.de.sentry.io/4509212154658896",
+ 
+ // Adds request headers and IP for users, for more info visit:
+ // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+ sendDefaultPii: true,
+ 
+ integrations: [nodeProfilingIntegration()],
+ tracesSampleRate: 1.0, // Capture 100% of the transactions
+ profilesSampleRate: 1.0, // profile every transaction
 });
