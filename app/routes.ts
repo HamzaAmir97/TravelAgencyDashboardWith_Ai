41133450b87fdf48d93c54d2e970fdc5b0253a30
Hmazah <alhamza2012@gmail.com>
2025-01-19 10:00:00 +0200

    add  google ai'

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,7 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
     route('sign_in', 'routes/root/sign_in.tsx'),
+    route('api/create-trip', 'routes/api/create-trip.ts'),
     layout('/routes/admin/admin_layout.tsx',[
 
         route('dashboard', 'routes/admin/dashboard.tsx'),
