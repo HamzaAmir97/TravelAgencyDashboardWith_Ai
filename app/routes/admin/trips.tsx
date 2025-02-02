b24886d60d2b04aac6d80ebbf78f19674f06161e
Hmazah <alhamza2012@gmail.com>
2025-02-02 10:00:00 +0200

    add  google ai'

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -1,7 +1,32 @@
-import { Header } from 'components'
+import {Header, TripCard} from "../../../components";
+import {type LoaderFunctionArgs, useSearchParams} from "react-router";
+import {getAllTrips, getTripById} from "~/appwrite/trips";
+import {parseTripData} from "~/lib/utils";
 
+import {useState} from "react";
+import {PagerComponent} from "@syncfusion/ej2-react-grids";
+import type { Route } from "../+types/home";
 
-function trips() {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+    const limit = 8;
+    const url = new URL(request.url);
+    const page = parseInt(url.searchParams.get('page') || "1", 10);
+    const offset = (page - 1) * limit;
+
+    const { allTrips, total } = await getAllTrips(limit, offset);
+
+    return {
+        trips: allTrips.map(({ $id, tripDetails, imageUrls }) => ({
+            id: $id,
+            ...parseTripData(tripDetails),
+            imageUrls: imageUrls ?? []
+        })),
+        total
+    }
+}
+
+const trips = ({ loaderData }: Route.ComponentProps) => {
+    const trips = (loaderData as any)?.trips || [];
   return (
       <Header
                 title="Trips"
