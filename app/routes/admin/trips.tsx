a878d0b1c76819204f908e5d76c79527296a660c
Hmazah <alhamza2012@gmail.com>
2025-02-03 10:00:00 +0200

    add  google ai'

--- app/routes/admin/trips.tsx
+++ app/routes/admin/trips.tsx
@@ -2,7 +2,6 @@ import {Header, TripCard} from "../../../components";
 import {type LoaderFunctionArgs, useSearchParams} from "react-router";
 import {getAllTrips, getTripById} from "~/appwrite/trips";
 import {parseTripData} from "~/lib/utils";
-
 import {useState} from "react";
 import {PagerComponent} from "@syncfusion/ej2-react-grids";
 import type { Route } from "../+types/home";
@@ -25,17 +24,56 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
     }
 }
 
-const trips = ({ loaderData }: Route.ComponentProps) => {
+const Trips = ({ loaderData }: Route.ComponentProps) => {
     const trips = (loaderData as any)?.trips || [];
-  return (
-      <Header
+
+    const [searchParams] = useSearchParams();
+    const initialPage = Number(searchParams.get('page') || '1')
+
+    const [currentPage, setCurrentPage] = useState(initialPage);
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+        window.location.search = `?page=${page}`
+    }
+
+    return (
+        <main className="all-users wrapper">
+            <Header
                 title="Trips"
                 description="View and edit AI-generated travel plans"
                 ctaText="Create a trip"
                 ctaUrl="/trips/create"
             />
 
-  )
-}
+            <section>
+                <h1 className="p-24-semibold text-dark-100 mb-4">
+                    Manage Created Trips
+                </h1>
 
-export default trips
+                <div className="trip-grid mb-4">
+                    {trips.map((trip: Trip) => (
+                        <TripCard
+                            key={trip.id}
+                            id={trip.id}
+                            name={trip.name}
+                            imageUrl={trip.imageUrls[0]}
+                            location={trip.itinerary?.[0]?.location ?? ""}
+                            tags={[trip.interests, trip.travelStyle]}
+                            price={trip.estimatedPrice}
+                        />
+                    ))}
+                </div>
+
+                <PagerComponent
+                    totalRecordsCount={(loaderData as any)?.total || 0}
+                    pageSize={8}
+                    currentPage={currentPage}
+                    click={(args) => handlePageChange(args.currentPage)}
+                    cssClass="!mb-4"
+                />
+            </section>
+        </main>
+    )
+}
+export default Trips
